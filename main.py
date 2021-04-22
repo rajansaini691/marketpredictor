a0c8198f349c0c36cd5a91d767e3948d333728b2
Rajan Saini <rajansaini@cs.ucsb.edu>
2021-04-21 17:01:27 -0700

    Quick-and-dirty point moving thing  - Code is pretty ugly rn; need to clean up before adding any more major features

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 from marketsegment import MarketSegment, MarketSegmentPlot
+from product import Product, ProductPlot
 
 import matplotlib
 matplotlib.use("TkAGG")
@@ -10,6 +11,51 @@ from tkinter import ttk
 import numpy as np
 
 
+# TODO Perhaps this should just do a single product, and the PerceptualMap can use
+#      an array of these guys
+class ProductGUI:
+    """
+    Takes care of the GUI elements that allow user to modify location of products
+    """
+
+    def __init__(self, axis, canvas, products, parent):
+        """
+        Puts the products onto the given pyplot axis and adds input elements
+        to the provided parent (tkinter)
+        """
+        # Use ProductPlot objects to draw each product to the given axis
+        self._product_plots = [ProductPlot(axis, p) for p in products]
+
+        # FIXME We're only dealing with one product for now; we'll scale later
+        self._single_product = self._product_plots[0]
+        x, y = self._single_product.get_location()
+
+        # Product info
+        product_x = tk.DoubleVar(value=x)
+        product_y = tk.DoubleVar(value=y)
+        product_input_x = ttk.Entry(parent, textvariable=product_x)
+        product_input_y = ttk.Entry(parent, textvariable=product_y)
+        product_input_x.pack()
+        product_input_y.pack()
+
+        product_input_x.bind('<Key-Return>', lambda _: self.update_product(axis, canvas, x=product_x.get()))
+        product_input_y.bind('<Key-Return>', lambda _: self.update_product(axis, canvas, y=product_y.get()))
+
+
+    # TODO Add entry boxes so we can use this function
+    # TODO Figure out why ax is being passed as a param and get rid of it
+    def update_product(self, ax, canvas, x=None, y=None, t=0):
+        """
+        Draw the desired point to the graph
+        """
+        assert(x is not None or y is not None or t is not None)
+        self._single_product.update_stats(t, size=y, performance=x)
+
+        canvas.draw()
+
+
+
+# TODO Figure out how to let time slider and products talk to each other
 class PerceptualMap(tk.Frame):
     """
     Draw the perceptual map graph and the needed sliders
@@ -18,7 +64,7 @@ class PerceptualMap(tk.Frame):
         A lot of this code was taken from a tutorial and can likely
         be improved
     """
-    def __init__(self, parent, controller, market_segments):
+    def __init__(self, parent, controller, num_years, market_segments, products):
         tk.Frame.__init__(self, parent)
 
         # The actual graph
@@ -35,8 +81,7 @@ class PerceptualMap(tk.Frame):
         m = (y1-y0)/(x1-x0)
         lin, = ax.plot([0, y0-m*x0], [x0-y0/m, 0])
 
-        # Add line going through initial size segment and in-between ideal
-        #x0,y0 = 
+        # TODO Add line going through initial size segment and in-between ideal
 
         # Grid line
         ax.minorticks_on()
@@ -59,15 +104,18 @@ class PerceptualMap(tk.Frame):
         canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
         # Allows the user to choose the point in time
-        time_slider = tk.Scale(self, 
-                from_=0, to=10, resolution=1, orient=tk.HORIZONTAL,
+        time_slider = tk.Scale(self,
+                from_=0, to=num_years, resolution=1, orient=tk.HORIZONTAL,
                 command=lambda t: self.update_time(int(t), canvas, market_segment_plots),
                 length=300)
 
         # Put the slider onto the window
         time_slider.pack()
 
+        # This thing deals with products
+        self._pg = ProductGUI(ax, canvas, products, self)
 
+        
     def update_time(self, t, canvas, market_segment_plots):
         """
         Draw the market segments to the graph
@@ -77,27 +125,7 @@ class PerceptualMap(tk.Frame):
 
         canvas.draw()
 
-
-    # TODO Add entry boxes so we can use this function
-    def update_point(self, ax, canvas, x=None, y=None):
-        """
-        Draw the desired point to the graph
-        """
-        assert(x is not None or y is not None)
-
-        help(self._point)
-        new_x, new_y = self._point
-
-        if x is not None:
-            new_x = x
-
-        if y is not None:
-            new_y = y
-
-        self._point = (new_x, new_y)
-        
-
-
+    
 class Window(tk.Tk):
     """
     Configure the actual window.
@@ -106,7 +134,7 @@ class Window(tk.Tk):
         A lot of this code was taken from a tutorial and can likely
         be improved
     """
-    def __init__(self, market_segments, *args, **kwargs):
+    def __init__(self, num_years, market_segments, products, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
 
         # Set the window title
@@ -119,11 +147,14 @@ class Window(tk.Tk):
         container.grid_columnconfigure(0, weight=1)
 
         # Draw the perceptual map
-        self.frame = PerceptualMap(container, self, market_segments)
+        self.frame = PerceptualMap(container, self, num_years, market_segments, products)
         self.frame.grid(row=0, column=0, sticky="nsew") # Something important
 
 
 if __name__ == "__main__":
+    # Constants
+    NUM_YEARS = 10
+
     # Create segments
     traditional = MarketSegment(
             starting_point=(5, 15), offset=(0, 0), drift=(0.7, -0.7),
@@ -142,25 +173,12 @@ if __name__ == "__main__":
             name="Size")
 
     market_segments = [low_end, traditional, high_end, performance, size]
+
+    # Create products
+    test = Product("test", 5, 5, 0, 1)
+
+    products = [test]
     
     # GUI code
-    app = Window(market_segments)
+    app = Window(NUM_YEARS, market_segments, products)
     app.mainloop()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- product.py
+++ product.py
@@ -13,7 +13,9 @@ class Product:
 
         # size[t], performance[t] gives the product's size/performance at year t
         self._size = [0 for _ in range(num_years)]
+        self._size[0] = size
         self._performance = [0 for _ in range(num_years)]
+        self._performance[0] = performance
 
         # Is the product actually in production?
         self._alive = True
@@ -31,10 +33,9 @@ class Product:
     def update_stats(self, t, size=None, performance=None):
         """
         Update the size and performance for the provided year t.
-        Note that t is currently unused, but will be changed in the future.
         """
         assert(size is not None or performance is not None)
-        assert(t > 0)
+        assert(t >= 0)
 
         if size is not None:
             self._size[t] = size
@@ -53,7 +54,7 @@ class Product:
         """
         return self._size[t] if t is not None else self._size[self._year]
 
-
+# TODO Implement
 class ProductPlot:
     """
     Draws a Product to a given matplotlib axis
@@ -66,4 +67,47 @@ class ProductPlot:
         """
         self._product = product
 
+        initial_size = product.get_size()
+        initial_performance = product.get_performance()
 
+        # Draw coordinate
+        self._product_coord = ax.scatter(initial_size, initial_performance)
+
+        # ax.scatter automatically assigns each spot a new color,
+        # so this guarantees that our colors will be unique
+        self._color = self._product_coord.get_edgecolor()
+
+        # Add text
+        self._name = ax.text(initial_size, initial_performance, product._name)
+
+    def update(self, t):
+        """
+        Draw the name and product coordinate
+        """
+        # Change the product's time
+        self._product.update_time(t)
+
+        # Update size and performance
+        size = self._product.get_size()
+        performance = self._product.get_performance()
+        self._product_coord.set_offsets((size, performance))
+
+        # Update name
+        self._name.set_position(size, performance)
+
+    # TODO There should be a better way to do this. Probably have ProductPlot
+    # and Product be siblings owned by ProductGUI, rather than children->grandchildren. IDK, though
+    def update_stats(self, t, size=None, performance=None):
+        self._product.update_stats(t, size=size, performance=performance)
+
+        # Update size and performance
+        size = self._product.get_size()
+        performance = self._product.get_performance()
+        self._product_coord.set_offsets((size, performance))
+
+        # Update name
+        self._name.set_position((size, performance))
+
+
+    def get_location(self):
+        return (self._product.get_performance(), self._product.get_size())
